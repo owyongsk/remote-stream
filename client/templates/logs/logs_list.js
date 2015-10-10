1b2a200c0c02944e66fe7afe55b25a5b2697afee
Edward Fung <edwardfung@redso.com.hk>
2015-10-10 18:53:10 +0800

    update logs UI.

--- client/templates/logs/logs_list.js
+++ client/templates/logs/logs_list.js
@@ -1,5 +1,10 @@
-var logsData = Logs.find();
+var logsData = Logs.find({}, {sort: {createdAt: -1}});
 
 Template.logsList.helpers({
   logs: logsData,
+});
+
+Template.registerHelper('formatDate', function(date, format){
+  //console.log(date);
+  return moment(date).format(format);
 });
