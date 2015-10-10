1494242845039eb56db5665fad15b4e160eedf73
Billy Cheung <longbcc112@Chung-Long-Cheungs-MacBook-Pro.local>
2015-10-10 17:46:29 +0800

    Update console.log

--- public/remote-stream.js
+++ public/remote-stream.js
@@ -1,19 +1,20 @@
-(function(){
-  oldLog = console.log;
-  window.console = {};
-  window.console.log = function(log) {
-    var r = new XMLHttpRequest();
-    r.open("POST", "http://localhost:3000/api/logs", true);
-    r.setRequestHeader('Content-Type', 'application/json');
-    r.onreadystatechange = function () {
-        if (r.readyState != 4 || r.status != 200) return;
-          alert("Success: " + r.responseText);
+console.log = (function() {
+    var log = console.log;
+
+    return function(data) {
+        var r = new XMLHttpRequest();
+         r.open("POST", "http://localhost:3000/api/logs", true);
+         r.setRequestHeader('Content-Type', 'application/json');
+         r.onreadystatechange = function () {
+             if (r.readyState != 4 || r.status != 200) return;
+               alert("Success: " + r.responseText);
+         };
+         data = {
+           "device": "SIDANLA",
+           "log": data
+         };
+         r.send(JSON.stringify(data));
+         log.call(console, data);
     };
-    data = {
-      "device": "SIDANLA",
-      "log": log
-    };
-    r.send(JSON.stringify(data));
-    oldLog.call(this, log);
-  };
 })();
+
