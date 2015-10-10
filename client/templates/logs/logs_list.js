09f9f72fe3343c12613651b31afa9b23bd607f64
Edward Fung <edwardfung@redso.com.hk>
2015-10-10 23:02:25 +0800

    Add severity filter and device filter.

--- client/templates/logs/logs_list.js
+++ client/templates/logs/logs_list.js
@@ -1,9 +1,42 @@
-var logsData = Logs.find({}, {sort: {createdAt: -1}});
+// Using Session to pass data around
+// See: http://stackoverflow.com/questions/27678923/filter-collection-on-dropdownlist-change
+Session.set('severity', '');
+Session.set('deviceid', '');
 
 Template.logsList.helpers({
-  logs: logsData,
+  logs: function(){
+    // Build the query from session data
+    var query = {};
+    var severity = Session.get('severity');
+    if (severity && severity != 'default'){
+      query.severity = severity;
+    }
+
+    var deviceid = Session.get('deviceid');
+    if (deviceid){
+      query.device = deviceid;
+    }
+    console.info(query);
+
+    return Logs.find(query, {sort: {createdAt: -1}});
+  },
 });
 
+// Handle UI events
+Template.logsFilter.events({
+  'change select[name=severity]': function(e, t){
+    Session.set('severity', $(e.currentTarget).val());
+    //console.info($(e.currentTarget).val());
+  },
+  'change input[name=deviceid]': function(e){
+    Session.set('deviceid', $(e.currentTarget).val());
+  },
+  'submit form': function(e){
+    e.preventDefault();
+  },
+});
+
+// Create filter
 Template.registerHelper('formatDate', function(date, format){
   //console.log(date);
   return moment(date).format(format);
--- public/remote-stream.js
+++ public/remote-stream.js
@@ -4,19 +4,20 @@ if(window.REMOTE_STREAM_USER) {
     var log = console.log;
 
     return function(msg) {
-        var r = new XMLHttpRequest();
-         r.open("POST", "http://localhost:3000/api/logs", true);
-         r.setRequestHeader('Content-Type', 'application/json');
-         r.onreadystatechange = function () {
-             if (r.readyState != 4 || r.status != 200) return;
-               alert("Success: " + r.responseText);
-         };
-         data = {
-           "device": window.REMOTE_STREAM_USER,
-           "log": msg
-         };
-         r.send(JSON.stringify(data));
-         log.call(console, msg);
+      var r = new XMLHttpRequest();
+      r.open("POST", "http://localhost:3000/api/logs", true);
+      r.setRequestHeader('Content-Type', 'application/json');
+      r.onreadystatechange = function () {
+        if (r.readyState != 4 || r.status != 200) return;
+        alert("Success: " + r.responseText);
+      };
+      data = {
+        "device": window.REMOTE_STREAM_USER,
+        "log": msg,
+        'severity': 'debug',
+      };
+      r.send(JSON.stringify(data));
+      log.call(console, msg);
     };
   })();
 
